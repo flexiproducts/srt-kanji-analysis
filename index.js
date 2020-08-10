9a4152bc107642ee9a18f9fd66ea5ea07d5aa03c
Finn Pauls <mbp@pauls.dev>
2020-08-10 18:21:38 +0200

    Show simple frequency after dropping file

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-const fs = require('fs')
-const isKanji = require("iskanji")
-const _ = require('lodash')
-const parser = require('subtitles-parser')
+import React, {useState} from 'react'
+import ReactDom from 'react-dom'
+import DropZone from 'react-drop-zone'
+import styled from 'styled-components'
+import isKanji from 'iskanji'
+import _ from 'lodash'
 
-const n = 10
+function App() {
+  const [frequentKanjis, setFrequentKanjis] = useState([])
 
-const remove = '春香渡邉香織松松嵜翔平奥山春花吉原健司田辺莉咲子西野入流佳玲奈徳井義実葉山奨之馬場園梓'
+  return (
+    <StyledDropZone onDrop={(file, text) => analyzeText(text)}>
+      {({over}) => (
+        <DropArea over={over}>
+          {frequentKanjis.map(([kanji, freq]) => (
+            <KanjiItem key={kanji}>
+              {kanji} {freq}
+            </KanjiItem>
+          ))}
+        </DropArea>
+      )}
+    </StyledDropZone>
+  )
 
-const subtitles = fs.readFileSync('subtitles.srt', 'utf-8')
-const subtitles2 = fs.readFileSync('subtitles2.srt', 'utf-8')
+  function analyzeText(text) {
+    setFrequentKanjis(getFrequentKanjis(text))
+  }
+}
 
-const lines = parser.fromSrt(subtitles).map(s => s.text)
+const KanjiItem = styled.div`
+  font-size: 3em;
+`
 
+const StyledDropZone = styled(DropZone)`
+  width: 100%;
+  min-height: 100%;
+`
 
-const sorted = getFrequentKanjis(subtitles)
-const sorted2 = getFrequentKanjis(subtitles2)
+const DropArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100%;
+  width: 100%;
+  background-color: ${({over}) => (over ? '#F4C1BD' : '#F4E8E7')};
+`
 
-
-console.log('number of different kanjis:', sorted.length)
-console.log('number of different kanjis 2:', sorted2.length)
-
-const kanji1 = sorted.map(sorted => sorted[0])
-const kanji2 = sorted2.map(sorted => sorted[0])
-
-console.log('new kanji in episode 2', kanji2.filter(kanji => !kanji1.includes(kanji)).length)
-
-
-const totalNumberOfKanjis = _.sumBy(sorted, pairs => pairs[1])
-console.log('total number', totalNumberOfKanjis)
-
-const firstN = sorted.slice(0, n)
-const numberOfFirstN = _.sumBy(firstN, pairs => pairs[1])
-console.log(`first ${n} number`, numberOfFirstN)
-
-const commonKanji = firstN.map(k => k[0])
-
-console.log(firstN)
-console.log('number of lines', lines.length)
-const linesWithKanji = lines.filter(line => line.split('').some(isKanji))
-console.log('lines with kanji', linesWithKanji.length)
-const withCommonKanjiLines = lines.filter(line => commonKanji.some(common => line.includes(common)))
-console.log('lines with common kanji', withCommonKanjiLines.length)
-console.log('all lines', withCommonKanjiLines.length/ lines.length * 100)
-console.log('only kanji lines', withCommonKanjiLines.length/ linesWithKanji.length * 100)
-
-
-sorted.slice(10, 20).forEach(([kanji, count]) => console.log(kanji, `https://jisho.org/search/${kanji}`, `https://hochanh.github.io/rtk/${kanji}`, count))
+ReactDom.render(<App />, document.getElementById('app'))
 
 function getFrequentKanjis(subtitles) {
-    const pairs = _.toPairs(_.countBy(subtitles.split('').filter(isKanji).filter(kanji => !remove.includes(kanji))))
-    const sorted = _.sortBy(pairs, ([kanji,count]) => -count)
-    return sorted
+  const pairs = _.toPairs(_.countBy(subtitles.split('').filter(isKanji)))
+  const sorted = _.sortBy(pairs, ([, count]) => -count)
+  return sorted
 }
