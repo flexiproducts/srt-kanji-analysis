7b39fd38163663bf5bf40c681469d184a35f8383
Finn Pauls <mbp@pauls.dev>
2020-08-11 00:10:01 +0200

    Add 3 facts about the kanji

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import ReactDom from 'react-dom'
 import styled from 'styled-components'
 import isKanji from 'iskanji'
 import {useDropzone} from 'react-dropzone'
-import _ from 'lodash'
+import _, {sumBy} from 'lodash'
 import {readAsText} from 'promise-file-reader'
+import parser from 'subtitles-parser'
 
 function App() {
   const [frequentKanjis, setFrequentKanjis] = useState([])
+  const [lines, setLines] = useState([])
+
   const {getRootProps, getInputProps, isDragActive} = useDropzone({
     onDrop,
     noClick: frequentKanjis.length > 0
@@ -16,28 +19,76 @@ function App() {
   return (
     <DropArea {...getRootProps()} over={isDragActive}>
       <input {...getInputProps()} />
-      {frequentKanjis.map(([kanji]) => (
-        <KanjiItem key={kanji}>
-          <Link
-            href={`https://hochanh.github.io/rtk/${kanji}/`}
-            target="heisig"
-          >
-            {kanji}
-          </Link>
-        </KanjiItem>
-      ))}
+      <Kanjis>
+        {frequentKanjis.map(([kanji]) => (
+          <KanjiItem key={kanji}>
+            <Link
+              href={`https://hochanh.github.io/rtk/${kanji}/`}
+              target="heisig"
+            >
+              {kanji}
+            </Link>
+          </KanjiItem>
+        ))}
+      </Kanjis>
+      {frequentKanjis.length > 0 && (
+        <Infos>
+          <Info>{frequentKanjis.length} different kanji</Info>
+          <KanjiLinePercentage {...{lines, frequentKanjis}} />
+          <KanjiFrequency frequentKanjis={frequentKanjis} />
+        </Infos>
+      )}
     </DropArea>
   )
 
   function onDrop(files) {
-    Promise.all(files.map(readAsText)).then((texts) =>
-      analyzeText(texts.join())
-    )
-  }
+    Promise.all(files.map(readAsText)).then((texts) => {
+      const allLines = texts
+        .map((text) => parser.fromSrt(text).map((s) => s.text))
+        .flat()
 
-  function analyzeText(text) {
-    setFrequentKanjis(getFrequentKanjis(text))
+      const kanjis = getFrequentKanjis(texts.join(''))
+
+      setLines(allLines)
+      setFrequentKanjis(kanjis)
+    })
   }
+}
+
+function KanjiFrequency({frequentKanjis}) {
+  const nMostCommon = 10
+  const firstN = frequentKanjis.slice(0, nMostCommon)
+
+  const totalNumberOfKanjis = sumBy(frequentKanjis, (pairs) => pairs[1])
+  const numberOfFirstN = sumBy(firstN, (pairs) => pairs[1])
+
+  const percentage = Math.round((numberOfFirstN / totalNumberOfKanjis) * 100)
+
+  return (
+    <Info>
+      The {nMostCommon} most common kanji make out {percentage}% of all kanji
+      usage
+    </Info>
+  )
+}
+
+function KanjiLinePercentage({lines, frequentKanjis}) {
+  const nMostCommon = 10
+
+  const firstN = frequentKanjis.slice(0, nMostCommon).map((k) => k[0])
+
+  const withCommonKanjiLines = lines.filter((line) =>
+    firstN.some((common) => line.includes(common))
+  )
+
+  const percentage = Math.round(
+    (withCommonKanjiLines.length / lines.length) * 100
+  )
+  return (
+    <Info>
+      The {nMostCommon} most common kanji are used in {percentage}% of lines
+    </Info>
+  )
 }
 
 const Link = styled.a`
@@ -45,16 +96,34 @@ const Link = styled.a`
   cursor: pointer;
 `
 
+const Infos = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f4c1bd;
+  padding: 2em;
+  font-family: sans-serif;
+  font-size: 2em;
+`
+
+const Info = styled.div`
+  margin-bottom: 2em;
+`
+
+const Kanjis = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap-reverse;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+`
+
 const KanjiItem = styled.div`
   font-size: 3em;
 `
 
 const DropArea = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap-reverse;
-  align-items: center;
-  height: 100%;
   width: 100%;
   background-color: ${({over}) => (over ? '#F4C1BD' : '#F4E8E7')};
 `
