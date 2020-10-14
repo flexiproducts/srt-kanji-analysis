ce823c2bbf93ab680db21b4fafac6e6e3170865d
Finn Pauls <mbp@pauls.dev>
2020-10-14 11:38:40 +0100

    Enable async await

--- index.tsx
+++ index.tsx
@@ -5,44 +5,44 @@ import {toPairs, sortBy, countBy, flatten} from 'lodash'
 import isKanji from 'iskanji'
 import {readAsText} from 'promise-file-reader'
 import parser from 'subtitles-parser'
+
 import Dropzone from './components/Dropzone'
 import Analysis from './components/Analysis'
-import {KanjiFrequencies, Lines} from './types'
 import Kanjis from './components/Kanjis'
+import {KanjiFrequencies, Lines} from './types'
 
 function App() {
   const [kanjiFrequencies, setKanjiFrequencies] = useState<KanjiFrequencies>([])
   const [lines, setLines] = useState<Lines>([])
 
+  const noKanjis = kanjiFrequencies.length === 0
+
   return (
-    <Dropzone noClick={kanjiFrequencies.length > 0} onDrop={onDrop}>
+    <Dropzone noClick={!noKanjis} onDrop={onDrop}>
       <Main>
-        {kanjiFrequencies.length === 0 ? (
+        {noKanjis ? (
           <Upload>Click or drag in a SRT file to analyze kanji usage</Upload>
         ) : (
           <Kanjis {...{kanjiFrequencies}} />
         )}
       </Main>
-      {kanjiFrequencies.length > 0 && (
-        <Analysis {...{kanjiFrequencies, lines}} />
-      )}
+      {!noKanjis && <Analysis {...{kanjiFrequencies, lines}} />}
     </Dropzone>
   )
 
-  function onDrop(files) {
-    Promise.all(files.map(readAsText)).then((texts) => {
-      const allLines = flatten(
-        texts.map((text) => parser.fromSrt(text).map((s) => s.text))
-      )
+  async function onDrop(files: File[]) {
+    const texts = await Promise.all(files.map(readAsText))
+    const allLines: Lines = flatten(
+      texts.map((text) => parser.fromSrt(text).map(({text}) => text))
+    )
 
-      const kanjis = getFrequentKanjis(texts.join(''))
+    const kanjis = calculateKanjiFrequencies(texts.join(''))
 
-      setLines(allLines)
-      setKanjiFrequencies(kanjis)
-    })
+    setLines(allLines)
+    setKanjiFrequencies(kanjis)
   }
 
-  function getFrequentKanjis(subtitles) {
+  function calculateKanjiFrequencies(subtitles: string): KanjiFrequencies {
     const pairs = toPairs(countBy(subtitles.split('').filter(isKanji)))
     const sorted = sortBy(pairs, ([, count]) => -count)
     return sorted
